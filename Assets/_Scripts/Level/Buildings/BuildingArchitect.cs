2fd13a427189b167787e463d147f85b4df7d2104
Pablo Flores <pfloresphoto@hotmail.com>
2022-09-21 22:51:41 -0300

    Update Alpha V0.4
    Update of room and building generators
    Adjusted door sizes
    Moved files to Resources Folder

---
+++ Assets/_Scripts/Level/Buildings/BuildingArchitect.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingArchitect : MonoBehaviour
+{
+    public enum BuildingStyle
+    {
+        Residential1,
+        Residential2,
+        Residential3,
+        ResidentialRuined,
+        Office,
+        Hospital,
+        Market,
+        PoliceStation,
+        FireStation,
+    }
+    public BuildingStyle buildingStyle;
+
+    public int buildingWidth;
+    public int buildingHeight;
+    public int floors;
+    public int roomsPerFloor;
+    public enum RoomType
+    {
+        Lobby,
+        StairsUp,
+        StairsDown,
+        EmptyRoom,
+        Kitchen,
+        Laundry,
+        Bedroom,
+        KidBedroom,
+        Living,
+        Bathroom,
+        OfficeBlocks,
+        OfficeBoss,
+        OfficeWarehouse,
+        ElevatorOut,
+        ElevatorIn,
+    }
+
+    public RoomType roomType;
+    
+ 
+
+    public RoomType[] buildOrder;
+
+    private BuildingGenerator _buildingGenerator;
+    
+    [HideInInspector]public string resourcePath;
+
+    private void OnValidate()
+    {
+        _buildingGenerator = GetComponent<BuildingGenerator>();
+        resourcePath = "Prefabs/Buildings/" + buildingStyle.ToString();
+        Debug.Log("Selected path: " + resourcePath);
+        switch (buildingStyle)
+        {
+            case BuildingStyle.Residential1:
+                
+                break;
+            
+            case BuildingStyle.Residential2:
+                
+                break;
+            
+            case BuildingStyle.Residential3:
+                
+                break;
+            
+            case BuildingStyle.ResidentialRuined:
+                
+                break;
+            
+            case BuildingStyle.Office:
+                
+                break;
+            
+            case BuildingStyle.Hospital:
+                
+                break;
+            
+            case BuildingStyle.Market:
+                
+                break;
+            
+            case BuildingStyle.PoliceStation:
+                
+                break;
+            
+            case BuildingStyle.FireStation:
+                
+                break;
+            
+        }
+    }
+}
---
+++ Assets/_Scripts/Level/Buildings/BuildingGenerator.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = System.Object;
+using Random = UnityEngine.Random;
+
+public class BuildingGenerator : MonoBehaviour
+{
+    
+    
+    public int maxWidth = 1;
+   public int maxHeight = 1;
+   public int maxDepth = 1;
+
+   public int minRoomWidth;
+   public int maxRoomWidth;
+   
+   public Transform spawnOrigin;
+   public Transform rightLimit;
+
+   public GameObject roomSeed;
+   private RoomGenerator _roomGen;
+   
+   public List<RoomGenerator> rooms;
+
+   private GameObject instRoom;
+
+   public Transform roomsTransform;
+
+   private MeshCombiner _meshCombiner;
+   
+   [SerializeField] private List<GameObject> doorPrefabList;
+
+   public BuildingArchitect architect;
+
+ 
+   private void Awake()
+   {
+    GetResources();
+   }
+
+   private void Start()
+   {
+       _meshCombiner = roomsTransform.GetComponent<MeshCombiner>();
+       rightLimit.position = Vector3.right * maxWidth * 3;
+       
+       for (int i = 0; i < maxHeight; i++)
+       {
+           for (int j = 0; j < maxWidth; j++)
+           {
+               instRoom = Instantiate(roomSeed, spawnOrigin.position, transform.rotation, roomsTransform);
+               _roomGen = instRoom.GetComponent<RoomGenerator>();
+               if (spawnOrigin.position.x < rightLimit.position.x)
+               { 
+                   _roomGen.StartGeneration(Random.Range(minRoomWidth, maxRoomWidth),1,  _roomGen.backDoorCount);
+                   spawnOrigin.position = _roomGen.rightExtent.position + Vector3.right * 1.45f;
+                   foreach (GameObject doorWall in _roomGen.doorWalls)
+                   {
+                       GameObject randDoorFile = doorPrefabList[Random.Range(0, doorPrefabList.Count)];
+                       GameObject instDoor = Instantiate(randDoorFile,doorWall.transform.position,doorWall.transform.rotation,transform);
+                   }
+               }
+           }
+           spawnOrigin.position = new Vector3(0,(i + 1) *_roomGen.pieceDimension,transform.position.z);
+           
+       }
+       CombineMeshes();
+   }
+   public void GetResources()
+   {
+       
+       Object[] doorPrefabsFolder = Resources.LoadAll("Prefabs/Doors", typeof(GameObject));
+       foreach (GameObject prefabFile in doorPrefabsFolder)
+       {
+           GameObject go = (GameObject) prefabFile;
+            
+           doorPrefabList.Add(go);
+       }  
+   }
+   public void CombineMeshes()
+   {
+       _meshCombiner.CreateMultiMaterialMesh = true;
+       _meshCombiner.CombineInactiveChildren = false;
+       _meshCombiner.DeactivateCombinedChildren = false;
+       _meshCombiner.DeactivateCombinedChildrenMeshRenderers = true;
+       _meshCombiner.GenerateUVMap = false;
+       _meshCombiner.DestroyCombinedChildren = false;
+       
+       _meshCombiner.CombineMeshes(false);
+   }
+}
