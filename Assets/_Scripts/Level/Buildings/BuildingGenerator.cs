aed33a17233782c344dc6fb41bcb4292dae9bd69
Pablo Flores <pfloresphoto@hotmail.com>
2023-02-18 10:42:23 -0300

    Update BuildingGenerator.cs

--- Assets/_Scripts/Level/Buildings/BuildingGenerator.cs
+++ Assets/_Scripts/Level/Buildings/BuildingGenerator.cs
@@ -172,12 +172,12 @@ public class BuildingGenerator : MonoBehaviour
                 else
                 {
                     //Dividimos las habitaciones en partes iguales segun cantidad de habitaciones por piso
-                    int[] roomsWidth = SortRooms(maxBldWidth,roomsPerFloor);                   
+<<<
                     //BUCLE (Horizontal X): desde 0 hasta la cantidad de habitaciones por piso  
                     for (int X = 0; X < roomsWidth.Length; X++)
                     {
                         //colocar un nuevo generador de habitacion y obtener su script
-                        RoomGenerator room = SetRoomSeed(spawnOrigin, roomsWidth[X]);
+<<<
                         RoomGenerator.RoomStyle roomName = (RoomGenerator.RoomStyle)Enum.Parse(
                             typeof(RoomGenerator.RoomStyle),
                             interiorPrefabs[Random.Range(0, interiorPrefabs.Length)].name);
